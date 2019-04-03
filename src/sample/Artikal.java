7093698432996656b7203e0c3c89ca75b1afe6cf
BakirK <bakir.karovic@gmail.com>
2019-04-03 09:30:41 +0200

    Dodani getteri / setteri i konstruktori u klasi Artikal

--- src/sample/Artikal.java
+++ src/sample/Artikal.java
@@ -3,9 +3,56 @@ package sample;
 public class Artikal {
         private String sifra, naziv;
         private double cijena;
-        public static  void lalla() {
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+
+    public Artikal(String sifraIn, String nazivIn, double cijenaIN) {
+        this.sifra = sifraIn;
+        this.naziv = nazivIn;
+        this.cijena = cijenaIN;
+    }
+    public Artikal (String inParameters) {
+        int i = 0;
+        for (String parameter: inParameters.split(",")) {
+            if (i == 0) {
+                this.sifra = parameter;
+                i++;
+            }
+            else if (i == 1) {
+                this.naziv = parameter;
+                i++;
+            }
+            else {
+                i = 0;
+                this.cijena = Integer.parseInt(parameter);
+            }
+
+
 
         }
-
+    }
 
 }
